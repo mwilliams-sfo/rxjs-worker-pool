22d402a9f31972d5a5feca4a5346780882984095
Myles Williams <mwilliams.sfo@gmail.com>
2024-04-26 22:51:44 -0700

    Factor WorkerPool to file

--- src/index.js
+++ src/index.js
@@ -1,47 +1,8 @@
-import * as rx from 'rxjs';
 import IterablePublisher from './flow/IterablePublisher';
 import tap from './flow/tap';
+import WorkerPool from './WorkerPool';
 import mapWithWorkers from './mapWithWorkers';
 
-class WorkerPool {
-	#workers;
-	#idleWorkers;
-
-	get idleCount() {
-		return this.#idleWorkers.pipe(rx.map(it => it.length));
-	}
-
-	constructor(count, workerFactory) {
-		if (count <= 0) throw new RangeError('WorkerPool size must be positive');
-
-		this.#workers = new Array(count);
-		for (let i = 0; i < count; i++) {
-			this.#workers[i] = workerFactory(i);
-		}
-		this.#idleWorkers = new rx.BehaviorSubject(this.#workers);
-	}
-
-	acquireWorker() {
-		return (async () => {
-			let idleWorkers;
-			while (true) {
-				idleWorkers = this.#idleWorkers.value;
-				if (idleWorkers.length) break;
-				await rx.firstValueFrom(
-					this.#idleWorkers.pipe(rx.first(it => it.length > 0)));
-			}
-			this.#idleWorkers.next(idleWorkers.slice(1));
-			return idleWorkers[0];
-		})();
-	}
-
-	releaseWorker(worker) {
-		if (this.#workers.indexOf(worker) < 0) throw new Error('Worker is not from this pool');
-		if (this.#idleWorkers.value.indexOf(worker) >= 0) throw new Error('Worker is already idle');
-
-		this.#idleWorkers.next([...this.#idleWorkers.value, worker]);
-	}
-}
 
 const poolSize = Math.max(1, (navigator.hardwareConcurrency ?? 1) - 1);
 document.querySelector('#poolSize').textContent = poolSize.toString();
