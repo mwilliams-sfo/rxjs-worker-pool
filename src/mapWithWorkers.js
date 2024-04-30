8d5b7086fd2f52d9c156b169fb0bc139123ec58f
Myles Williams <mwilliams.sfo@gmail.com>
2024-04-29 19:00:44 -0700

    Use queues to manage worker dispatch

--- src/mapWithWorkers.js
+++ src/mapWithWorkers.js
@@ -4,11 +4,16 @@ class MapWithWorkersSubscription {
 	#pool;
 	#subscriber;
 
+	#cancelled = false;
+	#demand = 0n;
 	#inputSubscription;
+	#inputError;
 	#inputComplete = false;
-	#terminated = false;
+
 	#inputQueue = [];
-	#processing = false;
+	#dispatching = false;
+	#taskQueue = [];
+	#collecting = false;
 
 	constructor(input, pool, subscriber) {
 		this.#input = input;
@@ -17,115 +22,164 @@ class MapWithWorkersSubscription {
 	}
 
 	cancel() {
-		if (this.#terminated) return;
-		this.#terminated = true;
-		this.#inputSubscription.cancel();
-		this.#inputQueue.length = 0;
+		if (this.#cancelled) return;
+		this.#cancelled = true;
+		this.#inputSubscription?.cancel();
+		this.#subscriber = null;
 	}
 
 	request(n) {
-		if (this.#terminated) return;
-		this.#ensureInputSubscription().then(() => {
-			if (this.#terminated) {
-				this.#inputSubscription.cancel();
-			} else {
-				this.#inputSubscription.request(n);
-			}
-		});
+		if (this.#cancelled) return;
+		try {
+			if (typeof n == 'number') n = BigInt(n);
+			if (n <= 0) throw new RangeError("Non-positive requests are not allowed.");
+			this.#demand += n;
+			(async () => {
+				try {
+					this.#inputSubscription ??= await this.#subscribeInput();
+					if (this.#cancelled) {
+						this.#inputSubscription.cancel();
+						return;
+					}
+					this.#inputSubscription.request(n);
+				} catch (err) {
+					this.#signalError(err);
+				}
+			})();
+		} catch (err) {
+			this.#signalError(err);
+		}
 	}
 
-	#ensureInputSubscription() {
-		return new Promise(resolve => {
-			if (this.#inputSubscription) return resolve();
-			this.#input.subscribe({
-				onSubscribe: subscription => {
-					this.#inputSubscription = subscription;
-					resolve();
-					if (this.#terminated) {
-						this.#inputSubscription.cancel();
-					} else {
-						this.#inputSubscription.request(n);
-					}
-				},
-				onNext: value => { this.#onNext(value);	},
-				onError: err => { this.#onError(err); },
-				onComplete: () => { this.#onComplete(); }
-			});
-		});
+	async #subscribeInput() {
+		const outer = this;
+		return new Promise(resolve => this.#input.subscribe({
+			onSubscribe(subscription) { resolve(subscription); },
+			onNext(value) { outer.#onNext(value); },
+			onError(err) { outer.#onError(err); },
+			onComplete() { outer.#onComplete(); }
+		}));
 	}
 
 	#onNext(value) {
-		if (this.#terminated) return;
 		this.#inputQueue.push(value);
-		if (this.#processing) return;
-		this.#processing = true;
-		this.#doProcessing();
+		this.#dispatch();
 	}
 
-	#doProcessing() {
-		this.#processing = true;
+	#onError(err) {
+		if (this.#dispatching || this.#collecting) {
+			this.#inputError = err;
+		} else {
+			this.#signalError(err);
+		}
+	}
+
+	#onComplete() {
+		if (this.#dispatching || this.#collecting) {
+			this.#inputComplete = true;
+		} else {
+			this.#signalComplete();
+		}
+	}
+
+	#dispatch() {
+		if (this.#dispatching) return;
+		this.#dispatching = true;
 		(async () => {
 			try {
-				while (this.#inputQueue.length > 0) {
+				while (!this.#cancelled && this.#inputQueue.length) {
 					const worker = await this.#pool.acquireWorker();
 					try {
-						const value = this.#inputQueue.shift();
-						const result = await this.#dispatchTo(worker, value);
-						this.#subscriber.onNext?.(result);
-					} finally {
+						this.#dispatchTo(worker, this.#inputQueue.shift());
+					} catch (err) {
 						this.#pool.releaseWorker(worker);
+						throw err;
 					}
 				}
-				if (!this.#terminated && this.#inputComplete) {
-					this.#terminated = true;
-					this.#subscriber.onComplete?.();
-				}
 			} catch (err) {
-				this.#terminated = true;
-				this.#subscriber.onError?.(err);
+				this.#signalError(err);
 			} finally {
-				this.#processing =  false;
+				this.#inputQueue.length = 0;
+				this.#dispatching = false;
 			}
 		})();
 	}
 
-	#onError(err) {
-		if (this.#terminated) return;
-		this.#terminated = true;
-		this.#subscriber.onError?.(err);
-	}
-
-	#onComplete() {
-		if (this.#terminated) return;
-		this.#inputComplete = true;
-		if (this.#inputQueue.length == 0) {
-			this.#terminated = true;
-			this.#subscriber.onComplete?.();
-		}
-	}
-
 	#dispatchTo(worker, value) {
-		return new Promise((resolve, reject) => {
-			let messageListener, messageErrorListener, errorListener;
-			const cleanup = () => {
+		let messageListener, messageErrorListener, errorListener;
+		const result =
+			Promise.race([
+				new Promise(resolve => {
+					worker.addEventListener('message', messageListener = evt => { resolve(evt.data); });
+				}),
+				new Promise((resolve, reject) => {
+					worker.addEventListener('messageerror', messageErrorListener = evt => {
+						reject(new Error('Worker message error'));
+					});
+				}),
+				new Promise((resolve, reject) => {
+					worker.addEventListener('error', errorListener = evt => {
+						reject(new Error('Worker error'));
+					});
+				})
+			]).finally(() => {
 				worker.removeEventListener('message', messageListener);
 				worker.removeEventListener('messageerror', messageErrorListener);
 				worker.removeEventListener('error', errorListener);
-			};
-			worker.addEventListener('message', messageListener = evt => {
-				resolve(evt.data);
-				cleanup();
 			});
-			worker.addEventListener('messageerror', messageErrorListener = evt => {
-				reject(new Error('Worker message error'));
-				cleanup();
-			});
-			worker.addEventListener('error', errorListener = evt => {
-				reject(new Error('Worker error'));
-				cleanup();
-			});
-			worker.postMessage(value);
-		});
+		worker.postMessage(value);
+		this.#taskQueue.push({worker, result});
+		this.#collect();
+	}
+
+	#collect() {
+		if (this.#collecting) return;
+		this.#collecting = true;
+		(async () => {
+			try {
+				while (this.#taskQueue.length) {
+					const task = this.#taskQueue.shift();
+					try {
+						const result = await task.result;
+						if (this.#cancelled) continue;
+						this.#signalNext(result);
+					} catch (err) {
+						this.#signalError(err);
+					} finally {
+						this.#pool.releaseWorker(task.worker);
+					}
+				}
+				if (this.#inputError) {
+					this.#signalError(this.#inputError);
+				} else if (this.#inputComplete) {
+					this.#signalComplete();
+				}
+			} finally {
+				this.#collecting = false;
+			}
+		})();
+	}
+
+	#signalNext(value) {
+		try {
+			this.#demand--;
+			this.#subscriber?.onNext(value);
+		} catch (err) {
+			this.cancel();
+			throw err;
+		}
+	}
+
+	#signalError(err) {
+		const subscriber = this.#subscriber;
+		this.cancel();
+		subscriber?.onError(err);
+	}
+
+	#signalComplete() {
+		const subscriber = this.#subscriber;
+		this.cancel();
+		subscriber?.onComplete();
 	}
 }
 
